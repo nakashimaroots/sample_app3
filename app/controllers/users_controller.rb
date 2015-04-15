8a266cc0075663864c2f6536ca79e2b0faad704c
nakashima_nitrous <nitrous@example.com>
2015-04-15 00:00:38 +0000

    0415

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,14 +21,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
+     if signed_in?
+      redirect_to root_url
+     else
+       @user = User.new(user_params)
+       if @user.save
+          sign_in @user
+          flash[:success] = "Welcome to the Sample App!"
+          redirect_to @user
+       else
+          render 'new'
+       end
+     end
   end
 
   def edit 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -91,6 +91,20 @@ describe "Authentication" do
         	    expect(page).to have_title('Edit user')
             end
           end
+          
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email", with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+            
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
+          end
 
           describe " in the Relationships controller" do
             describe "submitting to the create action" do
@@ -155,5 +169,15 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(root_path) }
         end
       end 
+    
+      describe "as admin user" do
+        let (:admin) { FactoryGirl.create(:admin)}
+        before { sign_in admin, no_capybara: true}
+        
+        describe "submitting a DELETE request to the admin#destroy"
+        
+          before { delete user_path(admin)}
+          specify { expect(response).to redirect_to(root_path)}
+      end
     end 
   end
